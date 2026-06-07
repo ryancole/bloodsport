9b8600f489683b0170bb6b77deb935524529b269
Ryan Cole <ryan@rycole.com>
2026-06-07 03:31:43 -0500

    add mig

---
+++ src/Libraries/Bloodsport.Data.Sql/Migrations/20260607083110_RosterMismatch.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bloodsport.Data.Sql.Migrations
+{
+    /// <inheritdoc />
+    public partial class RosterMismatch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "RosterMismatch",
+                table: "SeasonWeekMatchupResults",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RosterMismatch",
+                table: "SeasonWeekMatchupResults");
+        }
+    }
+}
