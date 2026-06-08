91575d5bc5cf49bf48e096c5738f00fd82658c8b
Ryan Cole <ryan@rycole.com>
2026-06-07 23:30:12 -0500

    modify run the func every hour

--- src/Compute/BloodsportFunctions/Functions/EndRegularSeasonWeek.cs
+++ src/Compute/BloodsportFunctions/Functions/EndRegularSeasonWeek.cs
@@ -18,7 +18,7 @@ public class EndRegularSeasonWeek
     }
 
     [Function("EndRegularSeasonWeek")]
-    public async Task Run([TimerTrigger("0 0 0 * * *")] TimerInfo myTimer)
+    public async Task Run([TimerTrigger("0 0 * * * *")] TimerInfo myTimer)
     {
         _logger.LogInformation("EndRegularSeasonWeek triggered at: {executionTime}", DateTime.UtcNow);
 
